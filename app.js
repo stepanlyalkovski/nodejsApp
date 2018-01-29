00f6c2abeed493e200f6098d07e90c756cc63750
Stsiapan_Lialkouski <Stsiapan_Lialkouski@epam.com>
2018-01-29 12:15:10 +0300

    add ejs and landing page, update default route handler

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var blogRouter = require('./blogRouter');
+var path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use('/blogs', blogRouter);
-
-// app.get('/', function(req, res) {
-//   res.send('Hello World!');
-// });
+app.use(function(req, res) {
+  res.render('index', {
+    url: req.originalUrl, 
+    method: req.method 
+  });
+});
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
