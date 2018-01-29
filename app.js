9c81d6cb0e1168b80ba7e42993b476b488b0e7d2
Stsiapan_Lialkouski <Stsiapan_Lialkouski@epam.com>
2018-01-29 12:18:02 +0300

    add some lost semicolon

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use('/blogs', blogRouter);
 app.use(function(req, res) {
   res.render('index', {
